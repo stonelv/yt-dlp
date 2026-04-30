1b425943adceb020ca6aa5e5b515776e18baee72
stonelv <zhe.lv.82@gmail.com>
2026-04-30 16:13:56 +0800

    audio by dogfooding round 1

--- yt_dlp/postprocessor/__init__.py
+++ yt_dlp/postprocessor/__init__.py
@@ -14,6 +14,7 @@ from .ffmpeg import (
     FFmpegFixupM4aPP,
     FFmpegFixupStretchedPP,
     FFmpegFixupTimestampPP,
+    FFmpegLoudnormPP,
     FFmpegMergerPP,
     FFmpegMetadataPP,
     FFmpegPostProcessor,
